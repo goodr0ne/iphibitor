3faef1812de8f4ea04f439f816903d951fb408a5
Yegor Scherbatkin <goodronechosen@gmail.com>
2018-11-19 21:48:22 +0300

    Add 502 response and Always Blocking Iphibitor class

--- src/main/java/goodr0ne/iphibitor/AlwaysBlockingIphibitor.java
+++ src/main/java/goodr0ne/iphibitor/AlwaysBlockingIphibitor.java
@@ -1,6 +1,6 @@
 package goodr0ne.iphibitor;
 
-public class AlwaysBlockingIphibitor {
+class AlwaysBlockingIphibitor {
 
   void inhibit() throws IphibitorRequestLimitReachedException {
     throw new IphibitorRequestLimitReachedException();
