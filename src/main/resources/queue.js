d5fee045487110b49faf1790245e10baa88e66b4
Yegor Scherbatkin <goodronechosen@gmail.com>
2018-11-20 15:44:05 +0300

    fix promises racing condition

--- src/main/resources/queue.js
+++ src/main/resources/queue.js
@@ -1,18 +1,19 @@
-const data = ["some", "data", "inputted", "here", "for", "you", "x", "will", "be",
+var data = ["some", "data", "inputted", "here", "for", "you", "x", "will", "be",
   "the", "16th", "symbol", "in", "this", "array", "x", "ha", "hah"];
 const limit = 3;
 var mapper = (a) => new Promise((resolve) => {
   console.out("mapper fired, a - " + a + ", resolve - " + resolve);
   setTimeout(resolve, Math.round(Math.random() * 900) + 100);
 });
-var results = [];
 
 function queue(a, b, c) {
+  var output = [];
   if (c > 1) {
     if (c > a.length) {
       for (var i = 0; i < a.length; i++) {
-        results.push(b([a[i]]));
+        output.push(b([a[i]]));
       }
+      return output;
     } else {
       const len = a.length;
       let j = 0;
@@ -20,16 +21,16 @@ function queue(a, b, c) {
         const size = Math.ceil((len - j) / c--);
         const part = a.slice(j, j += size);
         console.log("part used - " + part);
-        results.push(b(part));
+        output.push(b(part));
       }
+      console.log("output - " + output);
+      return output;
     }
   } else {
-    results.push(b(a));
+    output.push(b(a));
   }
 }
 
-queue.then(function(value) {
-  results.push(value);
+Promise.race(queue(data, mapper, limit)).then(function(result) {
+  console.log("results - " + result);
 });
-
-queue(data, mapper, limit);
